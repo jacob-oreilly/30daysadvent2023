1da52f7f6fe17c4da97fc99d8067e9f3a5094e09
Jake <jakeor45@gmail.com>
2023-12-03 19:30:43 -0800

    adding in check for lines with no numbers

--- adventday1/src/Main.java
+++ adventday1/src/Main.java
@@ -1,36 +1,38 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("assets/adventday1.txt");
         Scanner scan = new Scanner(file);
 
         int finalSum = 0;
+        int count = 0;
 
         while(scan.hasNextLine()) {
             String firstInt = "";
             String lastInt = "";
             String concatNumber = "";
-            char[] currentLine = scan.nextLine().toCharArray();
-            for(char character: currentLine) {
-                if(Character.isDigit(character)) {
-                    concatNumber += Character.toString(character);
-                    break;
+            String currentLineString = scan.nextLine();
+            char[] currentLineArray = currentLineString.toCharArray();
+            if(currentLineString.matches(".*\\d.*")) {
+                for(char character: currentLineArray) {
+                    if(Character.isDigit(character)) {
+                        concatNumber += Character.toString(character);
+                        break;
+                    }
+                }
+                for(int i = currentLineArray.length - 1; i > 0; i--) {
+                    if(Character.isDigit(currentLineArray[i])) {
+                        concatNumber += Character.toString(currentLineArray[i]);
+                        break;
+                    }
                 }
             }
-            for(int i = currentLine.length - 1; i > 0; i-- ) {
-                if(Character.isDigit(currentLine[i])) {
-                    concatNumber += Character.toString(currentLine[i]);
-                    break;
-                }
-            }
-            System.out.println(concatNumber);
 
-            finalSum += Integer.parseInt(concatNumber);
+            if (!concatNumber.isEmpty()) {
+                finalSum += Integer.parseInt(concatNumber);
+            }
             System.out.println(finalSum);
         }
 
